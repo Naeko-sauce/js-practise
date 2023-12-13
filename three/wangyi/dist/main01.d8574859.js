5330269d434dfd4448825989898339116cd2d4b4
Naeko-sauce <3443471283@qq.com>
2023-12-13 23:52:16 +0800

    配置

--- three/wangyi/dist/main01.d8574859.js
+++ three/wangyi/dist/main01.d8574859.js
@@ -38971,7 +38971,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45667" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
