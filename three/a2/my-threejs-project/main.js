2e67e8f756448821c91418f9b759070fab460c9a
Naeko-sauce <3443471283@qq.com>
2023-12-13 23:52:33 +0800

    删除three

--- three/a2/my-threejs-project/main.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-import { setupCounter } from "./counter.js";
-import MainThree from "./MainThree.js";
-document.querySelector("#app");
-MainThree();
-setupCounter(document.querySelector("#counter"));
